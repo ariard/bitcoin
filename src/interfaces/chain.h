354fb817363c96ad3af0df6639e8d54b74388667
Antoine Riard <ariard@student.42.fr>
2019-08-08 14:12:03 -0400

    Extend Chain interface to register/start/stop notifications
    Chain interface callers may register for notifications while
    initializing themselves. Init code starts notification delivery,
    shutdown code is responsible to stop it.
    
    Notification delivery runs in its own thread (ThreadServiceRequests)
    during rescan period then rely on CValidationInterface.

--- src/interfaces/chain.h
+++ src/interfaces/chain.h
@@ -20,6 +20,7 @@ class CRPCCommand;
 class CScheduler;
 class CValidationState;
 class Coin;
+class Rescan;
 class uint256;
 enum class RBFTransactionState;
 struct CBlockLocator;
@@ -240,6 +241,18 @@ public:
     //! Register handler for notifications.
     virtual std::unique_ptr<Handler> handleNotifications(Notifications& notifications) = 0;
 
+    //! Start notifications sequence in order for every ChainClient, XXX:should only be called by init code.
+    virtual void startNotifications() = 0;
+
+    //! Stop notifications sequence, XXX:should only be called by stop code.
+    virtual void stopNotifications() = 0;
+
+    //! Interrupt notification sequence, XXX: should only be called by stop code.
+    virtual void interruptNotifications() = 0;
+
+    //! Request notifications.
+    virtual void registerNotifications(Notifications& callback, const CBlockLocator& locator) = 0;
+
     //! Wait for pending notifications to be processed unless block hash points to the current
     //! chain tip, or to a possible descendant of the current chain tip that isn't currently
     //! connected.
@@ -295,7 +308,6 @@ public:
     virtual void stop() = 0;
 };
 
-//! Return implementation of Chain interface.
 std::unique_ptr<Chain> MakeChain();
 
 //! Return implementation of ChainClient interface for a wallet client. This
