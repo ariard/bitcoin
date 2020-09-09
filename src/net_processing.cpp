b617e3917c430af73ba049b9148aaecb6f27e691
Carl Dong <contact@carldong.me>
2020-09-09 16:05:39 -0400

    validation: Pass in spendheight to CTxMemPool::check
    This is the only instance where validation reaches for something outside
    of it.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2048,7 +2048,9 @@ void PeerManager::ProcessOrphanTx(std::set<uint256>& orphan_work_set)
             break;
         }
     }
-    m_mempool.check(&::ChainstateActive().CoinsTip());
+    CChainState& active_chainstate = m_chainman.ActiveChainstate();
+    CCoinsViewCache& active_coins_tip = active_chainstate.CoinsTip();
+    m_mempool.check(&active_coins_tip, active_chainstate.m_blockman.GetSpendHeight(active_coins_tip));
 }
 
 /**
@@ -2980,7 +2982,10 @@ void PeerManager::ProcessMessage(CNode& pfrom, const std::string& msg_type, CDat
         std::list<CTransactionRef> lRemovedTxn;
 
         if (AcceptToMemoryPool(::ChainstateActive(), m_mempool, state, ptx, &lRemovedTxn, false /* bypass_limits */)) {
-            m_mempool.check(&::ChainstateActive().CoinsTip());
+            CChainState& active_chainstate = m_chainman.ActiveChainstate();
+            CCoinsViewCache& active_coins_tip = active_chainstate.CoinsTip();
+            m_mempool.check(&active_coins_tip, active_chainstate.m_blockman.GetSpendHeight(active_coins_tip));
+
             // As this version of the transaction was acceptable, we can forget about any
             // requests for it.
             m_txrequest.ForgetTxHash(tx.GetHash());
