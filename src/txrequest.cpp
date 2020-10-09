93d6a00a2da5eace70afd2e794a772a7c9be541a
Pieter Wuille <pieter@wuille.net>
2020-10-09 12:48:43 -0700

    Remove support for time going backwards

--- src/txrequest.cpp
+++ src/txrequest.cpp
@@ -176,14 +176,11 @@ enum class WaitState {
     FUTURE_EVENT,
     //! Used for announcements whose timestamp is not relevant.
     NO_EVENT,
-    //! Used for announcements that need efficient testing of "is their timestamp in the past?".
-    PAST_EVENT,
 };
 
 WaitState GetWaitState(const Announcement& ann)
 {
     if (ann.IsWaiting()) return WaitState::FUTURE_EVENT;
-    if (ann.IsSelectable()) return WaitState::PAST_EVENT;
     return WaitState::NO_EVENT;
 }
 
@@ -195,7 +192,6 @@ WaitState GetWaitState(const Announcement& ann)
 // Uses:
 // * Finding CANDIDATE_DELAYED announcements whose reqtime has passed, and REQUESTED announcements whose expiry has
 //   passed.
-// * Finding CANDIDATE_READY/BEST announcements whose reqtime is in the future (when the clock time went backwards).
 struct ByTime {};
 using ByTimeView = std::pair<WaitState, std::chrono::microseconds>;
 struct ByTimeViewExtractor
@@ -313,6 +309,9 @@ class TxRequestTracker::Impl {
     //! Map with this tracker's per-peer statistics.
     std::unordered_map<NodeId, PeerInfo> m_peerinfo;
 
+    //! The current time. It is kept here internally to guarantee a view of time that does not go backwards.
+    std::chrono::microseconds m_now = std::chrono::microseconds::min();
+
 public:
     void SanityCheck() const
     {
@@ -348,17 +347,17 @@ public:
         }
     }
 
-    void PostGetRequestableSanityCheck(std::chrono::microseconds now) const
+    void PostGetRequestableSanityCheck() const
     {
         for (const Announcement& ann : m_index) {
             if (ann.IsWaiting()) {
                 // REQUESTED and CANDIDATE_DELAYED must have a time in the future (they should have been converted
                 // to COMPLETED/CANDIDATE_READY respectively).
-                assert(ann.m_time > now);
+                assert(ann.m_time > m_now);
             } else if (ann.IsSelectable()) {
-                // CANDIDATE_READY and CANDIDATE_BEST cannot have a time in the future (they should have remained
-                // CANDIDATE_DELAYED, or should have been converted back to it if time went backwards).
-                assert(ann.m_time <= now);
+                // CANDIDATE_READY and CANDIDATE_BEST cannot have a time in the future, as it would imply m_now
+                // going backwards.
+                assert(ann.m_time <= m_now);
             }
         }
     }
@@ -478,35 +477,23 @@ private:
         return true;
     }
 
-    //! Make the data structure consistent with a given point in time:
+    //! Move our clock forward and make the data structure consistent with the new time.
     //! - REQUESTED annoucements with expiry <= now are turned into COMPLETED.
     //! - CANDIDATE_DELAYED announcements with reqtime <= now are turned into CANDIDATE_{READY,BEST}.
-    //! - CANDIDATE_{READY,BEST} announcements with reqtime > now are turned into CANDIDATE_DELAYED.
     void SetTimePoint(std::chrono::microseconds now, std::vector<std::pair<NodeId, GenTxid>>* expired)
     {
         if (expired) expired->clear();
+        m_now = std::max(m_now, now);
 
         // Iterate over all CANDIDATE_DELAYED and REQUESTED from old to new, as long as they're in the past,
         // and convert them to CANDIDATE_READY and COMPLETED respectively.
         while (!m_index.empty()) {
             auto it = m_index.get<ByTime>().begin();
-            if (it->m_state == State::CANDIDATE_DELAYED && it->m_time <= now) {
+            if (it->m_state == State::CANDIDATE_DELAYED && it->m_time <= m_now) {
                 PromoteCandidateReady(m_index.project<ByTxHash>(it));
-            } else if (it->m_state == State::REQUESTED && it->m_time <= now) {
+            } else if (it->m_state == State::REQUESTED && it->m_time <= m_now) {
                 if (expired) expired->emplace_back(it->m_peer, ToGenTxid(*it));
                 MakeCompleted(m_index.project<ByTxHash>(it));
-            } else {
-                break;
-            }
-        }
-
-        while (!m_index.empty()) {
-            // If time went backwards, we may need to demote CANDIDATE_BEST and CANDIDATE_READY announcements back
-            // to CANDIDATE_DELAYED. This is an unusual edge case, and unlikely to matter in production. However,
-            // it makes it much easier to specify and test TxRequestTracker::Impl's behaviour.
-            auto it = std::prev(m_index.get<ByTime>().end());
-            if (it->IsSelectable() && it->m_time > now) {
-                ChangeAndReselect(m_index.project<ByTxHash>(it), State::CANDIDATE_DELAYED);
             } else {
                 break;
             }
@@ -702,9 +689,9 @@ size_t TxRequestTracker::Count(NodeId peer) const { return m_impl->Count(peer); 
 size_t TxRequestTracker::Size() const { return m_impl->Size(); }
 void TxRequestTracker::SanityCheck() const { m_impl->SanityCheck(); }
 
-void TxRequestTracker::PostGetRequestableSanityCheck(std::chrono::microseconds now) const
+void TxRequestTracker::PostGetRequestableSanityCheck() const
 {
-    m_impl->PostGetRequestableSanityCheck(now);
+    m_impl->PostGetRequestableSanityCheck();
 }
 
 void TxRequestTracker::ReceivedInv(NodeId peer, const GenTxid& gtxid, bool preferred,
