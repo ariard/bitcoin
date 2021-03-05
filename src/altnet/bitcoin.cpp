c589c6f9dc98bdccc7713116765243324305b7df
Antoine Riard <antoine.riard@gmail.com>
2021-03-05 10:11:02 -0500

    tmp: daemon improvements

--- src/altnet/bitcoin.cpp
+++ src/altnet/bitcoin.cpp
@@ -2,6 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <chainparamsbase.h>
 #include <interfaces/init.h>
 #include <util/strencodings.h>
 #include <util/system.h>
@@ -15,14 +16,28 @@ const std::function<std::string(const char*)> G_TRANSLATION_FUN = nullptr;
 int main(int argc, char* argv[])
 {
     int exit_status;
-    //std::unique_ptr<interfaces::Init> init = interfaces::MakeAltnetInit(argc, argv, exit_status);
-    //if (!init) {
-    //    return exit_status;
-    //}
 
     SetupEnvironment();
-    //TODO?
-    LogPrintf("Altnet started!\n");
 
+    SelectBaseParams(CBaseChainParams::MAIN);
+
+    LogInstance().m_print_to_file = true;
+    //LogInstance().m_file_path = "/home/user/.bitcoin/debug-altnet.log";
+    LogInstance().m_file_path = "debug-altnet.log";
+    LogInstance().m_print_to_console = false;
+    LogInstance().m_log_timestamps = DEFAULT_LOGTIMESTAMPS;
+    LogInstance().m_log_time_micros = DEFAULT_LOGTIMEMICROS;
+    LogInstance().m_log_threadnames = DEFAULT_LOGTHREADNAMES;
+
+    if (!LogInstance().StartLogging()) {
+        throw std::runtime_error(strprintf("Could not open debug log file %s", LogInstance().m_file_path.string()));
+    }
+
+    std::unique_ptr<interfaces::Init> init = interfaces::MakeAltnetInit(argc, argv, exit_status);
+    if (!init) {
+        return exit_status;
+    }
+
+    LogPrintf("This is Altnet!");
     while (true) {}
 }
