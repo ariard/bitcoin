2fe5b27fb2b2c7f0e9a5dc309b7aa3a6680998b7
Antoine Riard <dev@ariard.me>
2021-07-05 09:52:12 -0400

    Add explicit logger for controlled process

--- src/ipc/capnp/protocol.cpp
+++ src/ipc/capnp/protocol.cpp
@@ -29,7 +29,7 @@ namespace capnp {
 namespace {
 void IpcLogFn(bool raise, std::string message)
 {
-    LogPrint(BCLog::IPC, "%s\n", message);
+    LogPrintf("%s\n", message);
     if (raise) throw Exception(message);
 }
 
